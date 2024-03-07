90465161f5c9a75b2f17f2a990948b16e74b4a34
hatef khodkar <hatef.khodkar@hotmail.com>
2024-03-07 20:27:33 +0400

    feat(No project): add no project content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import NewProject from "./components/NewProject";
+import { useState } from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
+
+  const [projectsState, setProjectStates] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectStates(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
